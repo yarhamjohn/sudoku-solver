e00ce5adb0c4bc649bb4058b8d51a24c1f1fa42d
John Yarham <yarhamjohn@gmail.com>
2020-07-17 20:52:23 +0100

    more tests

--- sudokuGridSolver_test.go
+++ sudokuGridSolver_test.go
@@ -29,7 +29,7 @@ func TestSolveGrid(t *testing.T) {
 		}
 	})
 
-	t.Run("CompletesGridWith_TwoEmptyBlocks_InDifferentRows_InSameColumn", func(t *testing.T) {
+	t.Run("CompletesGridWith_TwoEmptyBlocks_InTwoRows_InOneColumn_InOneSquare", func(t *testing.T) {
 		gridToTest := sudokuGrid{
 			createBlocks([]string{" ", "3", "4", "6", "7", "8", "9", "1", "2"}),
 			createBlocks([]string{" ", "7", "2", "1", "9", "5", "3", "4", "8"}),
@@ -57,7 +57,7 @@ func TestSolveGrid(t *testing.T) {
 		}
 	})
 
-	t.Run("CompletesGridWith_TwoEmptyBlocks_InDifferentColumns_InSameRow", func(t *testing.T) {
+	t.Run("CompletesGridWith_TwoEmptyBlocks_InTwoColumns_InOneRow_InOneSquare", func(t *testing.T) {
 		gridToTest := sudokuGrid{
 			createBlocks([]string{" ", " ", "4", "6", "7", "8", "9", "1", "2"}),
 			createBlocks([]string{"6", "7", "2", "1", "9", "5", "3", "4", "8"}),
@@ -85,7 +85,7 @@ func TestSolveGrid(t *testing.T) {
 		}
 	})
 
-	t.Run("CompletesGridWith_TwoEmptyBlocks_InDiffentRows_InDifferentColumns_InSameSquare", func(t *testing.T) {
+	t.Run("CompletesGridWith_TwoEmptyBlocks_InTwoRows_InTwoColumns_InOneSquare", func(t *testing.T) {
 		gridToTest := sudokuGrid{
 			createBlocks([]string{" ", "3", "4", "6", "7", "8", "9", "1", "2"}),
 			createBlocks([]string{"6", " ", "2", "1", "9", "5", "3", "4", "8"}),
@@ -113,7 +113,7 @@ func TestSolveGrid(t *testing.T) {
 		}
 	})
 
-	t.Run("CompletesGridWith_TwoEmptyBlocks_InDiffentRows_InDifferentColumns_InDifferentSquares", func(t *testing.T) {
+	t.Run("CompletesGridWith_TwoEmptyBlocks_InTwoRows_InTwoColumns_InTwoSquares", func(t *testing.T) {
 		gridToTest := sudokuGrid{
 			createBlocks([]string{" ", "3", "4", "6", "7", "8", "9", "1", "2"}),
 			createBlocks([]string{"6", "7", "2", " ", "9", "5", "3", "4", "8"}),
@@ -141,7 +141,7 @@ func TestSolveGrid(t *testing.T) {
 		}
 	})
 
-	t.Run("CompletesGridWith_FourEmptyBlocks_InTwoRows_InTwoColumns_InDifferentSquares", func(t *testing.T) {
+	t.Run("CompletesGridWith_FourEmptyBlocks_InTwoRows_InTwoColumns_InOneSquare", func(t *testing.T) {
 		gridToTest := sudokuGrid{
 			createBlocks([]string{" ", "3", "4", " ", "7", "8", "9", "1", "2"}),
 			createBlocks([]string{"6", "7", "2", "1", "9", "5", "3", "4", "8"}),
@@ -177,7 +177,7 @@ func TestSolveGrid(t *testing.T) {
 		}
 	})
 
-	t.Run("CompletesGridWith_FourEmptyBlocks_InTwoRows_InTwoColumns_InSameSquare", func(t *testing.T) {
+	t.Run("CompletesGridWith_FourEmptyBlocks_InTwoRows_InTwoColumns_InOneSquare", func(t *testing.T) {
 		gridToTest := sudokuGrid{
 			createBlocks([]string{" ", "3", " ", "6", "7", "8", "9", "1", "2"}),
 			createBlocks([]string{"6", "7", "2", "1", "9", "5", "3", "4", "8"}),
@@ -242,6 +242,106 @@ func TestSolveGrid(t *testing.T) {
 
 		if gridToTest[2][3].GetBlockValue() != "3" {
 			t.Errorf("Block [2][3]] should have value 3 but had value " + gridToTest[2][3].GetBlockValue())
+		}
+
+		if !gridIsComplete(&gridToTest) {
+			t.Errorf("The gridToTest should be complete, but is not: \n" + gridToTest.String())
+		}
+	})
+
+	t.Run("CompletesGridWith_SixEmptyBlocks_InThreeRows_InTwoColumns_InOneSquare", func(t *testing.T) {
+		gridToTest := sudokuGrid{
+			createBlocks([]string{" ", "3", " ", "6", "7", "8", "9", "1", "2"}),
+			createBlocks([]string{" ", "7", " ", "1", "9", "5", "3", "4", "8"}),
+			createBlocks([]string{" ", "9", " ", "3", "4", "2", "5", "6", "7"}),
+			createBlocks([]string{"8", "5", "9", "7", "6", "1", "4", "2", "3"}),
+			createBlocks([]string{"4", "2", "6", "8", "5", "3", "7", "9", "1"}),
+			createBlocks([]string{"7", "1", "3", "9", "2", "4", "8", "5", "6"}),
+			createBlocks([]string{"9", "6", "1", "5", "3", "7", "2", "8", "4"}),
+			createBlocks([]string{"2", "8", "7", "4", "1", "9", "6", "3", "5"}),
+			createBlocks([]string{"3", "4", "5", "2", "8", "6", "1", "7", "9"}),
+		}
+
+		SolveGrid(&gridToTest)
+
+		if gridToTest[0][0].GetBlockValue() != "5" {
+			t.Errorf("Block [0][0] should have value 5 but had value " + gridToTest[0][0].GetBlockValue())
+		}
+
+		if gridToTest[1][0].GetBlockValue() != "6" {
+			t.Errorf("Block [1][0] should have value 6 but had value " + gridToTest[1][0].GetBlockValue())
+		}
+
+		if gridToTest[2][0].GetBlockValue() != "1" {
+			t.Errorf("Block [2][0] should have value 1 but had value " + gridToTest[2][0].GetBlockValue())
+		}
+
+		if gridToTest[0][2].GetBlockValue() != "4" {
+			t.Errorf("Block [0][2]] should have value 4 but had value " + gridToTest[0][2].GetBlockValue())
+		}
+
+		if gridToTest[1][2].GetBlockValue() != "2" {
+			t.Errorf("Block [1][2]] should have value 2 but had value " + gridToTest[1][2].GetBlockValue())
+		}
+
+		if gridToTest[2][2].GetBlockValue() != "8" {
+			t.Errorf("Block [2][2]] should have value 8 but had value " + gridToTest[2][2].GetBlockValue())
+		}
+
+		if !gridIsComplete(&gridToTest) {
+			t.Errorf("The gridToTest should be complete, but is not: \n" + gridToTest.String())
+		}
+	})
+
+	t.Run("CompletesGridWith_NineEmptyBlocks_InThreeRows_InThreeColumns_InOneSquare", func(t *testing.T) {
+		gridToTest := sudokuGrid{
+			createBlocks([]string{" ", " ", " ", "6", "7", "8", "9", "1", "2"}),
+			createBlocks([]string{" ", " ", " ", "1", "9", "5", "3", "4", "8"}),
+			createBlocks([]string{" ", " ", " ", "3", "4", "2", "5", "6", "7"}),
+			createBlocks([]string{"8", "5", "9", "7", "6", "1", "4", "2", "3"}),
+			createBlocks([]string{"4", "2", "6", "8", "5", "3", "7", "9", "1"}),
+			createBlocks([]string{"7", "1", "3", "9", "2", "4", "8", "5", "6"}),
+			createBlocks([]string{"9", "6", "1", "5", "3", "7", "2", "8", "4"}),
+			createBlocks([]string{"2", "8", "7", "4", "1", "9", "6", "3", "5"}),
+			createBlocks([]string{"3", "4", "5", "2", "8", "6", "1", "7", "9"}),
+		}
+
+		SolveGrid(&gridToTest)
+
+		if gridToTest[0][0].GetBlockValue() != "5" {
+			t.Errorf("Block [0][0] should have value 5 but had value " + gridToTest[0][0].GetBlockValue())
+		}
+
+		if gridToTest[1][0].GetBlockValue() != "6" {
+			t.Errorf("Block [1][0] should have value 6 but had value " + gridToTest[1][0].GetBlockValue())
+		}
+
+		if gridToTest[2][0].GetBlockValue() != "1" {
+			t.Errorf("Block [2][0] should have value 1 but had value " + gridToTest[2][0].GetBlockValue())
+		}
+
+		if gridToTest[0][1].GetBlockValue() != "3" {
+			t.Errorf("Block [0][1]] should have value 3 but had value " + gridToTest[0][1].GetBlockValue())
+		}
+
+		if gridToTest[1][1].GetBlockValue() != "7" {
+			t.Errorf("Block [1][1]] should have value 7 but had value " + gridToTest[1][1].GetBlockValue())
+		}
+
+		if gridToTest[2][1].GetBlockValue() != "9" {
+			t.Errorf("Block [2][1]] should have value 9 but had value " + gridToTest[2][1].GetBlockValue())
+		}
+
+		if gridToTest[0][2].GetBlockValue() != "4" {
+			t.Errorf("Block [0][2]] should have value 4 but had value " + gridToTest[0][2].GetBlockValue())
+		}
+
+		if gridToTest[1][2].GetBlockValue() != "2" {
+			t.Errorf("Block [1][2]] should have value 2 but had value " + gridToTest[1][2].GetBlockValue())
+		}
+
+		if gridToTest[2][2].GetBlockValue() != "8" {
+			t.Errorf("Block [2][2]] should have value 8 but had value " + gridToTest[2][2].GetBlockValue())
 		}
 
 		if !gridIsComplete(&gridToTest) {
