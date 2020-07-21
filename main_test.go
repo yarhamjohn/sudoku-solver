b5f479cb2ddcff5e25ea2e90848feb55fa3e1f83
John Yarham <yarhamjohn@gmail.com>
2020-07-21 21:22:18 +0100

    add further tests

--- main_test.go
+++ main_test.go
@@ -34,15 +34,66 @@ func TestSudokuSolver(t *testing.T) {
 			" ", "6", "2", "9", " ", "8", "1", "4", " ",
 		}
 
-		fmt.Println(strings.Join(input, ","))
-
 		cmd := exec.Command("./sudoku-solver.exe", "--grid", strings.Join(input, ","))
 
-		output, err := cmd.CombinedOutput()
+		output, err := cmd.Output()
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		fmt.Println(output)
+		if !strings.Contains(string(output), "The grid has been solved!") {
+			t.Errorf("Grid should have been solved, but was not: \n" + string(output))
+		}
+	})
+
+	t.Run("SolvesMediumSudokuGrid", func(t *testing.T) {
+		input := []string{
+			" ", " ", " ", " ", " ", "3", " ", " ", " ",
+			" ", "5", " ", "1", "6", " ", "7", " ", " ",
+			" ", " ", " ", " ", " ", "9", "8", "2", " ",
+			" ", " ", " ", "4", " ", " ", "3", " ", "5",
+			" ", "3", "9", "8", " ", " ", " ", "1", " ",
+			"5", " ", "4", " ", "3", "6", " ", "8", " ",
+			"7", "4", " ", "3", "2", " ", " ", " ", " ",
+			" ", " ", "6", " ", "1", " ", " ", "4", " ",
+			" ", " ", "8", "6", " ", " ", " ", " ", " ",
+		}
+
+		cmd := exec.Command("./sudoku-solver.exe", "--grid", strings.Join(input, ","))
+
+		output, err := cmd.Output()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !strings.Contains(string(output), "The grid has been solved!") {
+			t.Errorf("Grid should have been solved, but was not: \n" + string(output))
+		}
+	})
+
+	t.Run("SolvesHardSudokuGrid", func(t *testing.T) {
+		input := []string{
+			" ", " ", " ", " ", " ", " ", " ", " ", " ",
+			" ", " ", " ", "8", " ", "7", " ", " ", " ",
+			" ", "5", "6", " ", "3", " ", "7", "8", " ",
+			" ", "6", "1", " ", "8", " ", "9", "5", " ",
+			"2", " ", " ", "6", " ", "5", " ", " ", "4",
+			" ", " ", " ", " ", " ", " ", " ", " ", " ",
+			"9", " ", " ", " ", "4", " ", " ", " ", "7",
+			"6", " ", "2", " ", "5", " ", "4", " ", "9",
+			"5", " ", "4", "2", " ", "1", "3", " ", "8",
+		}
+
+		cmd := exec.Command("./sudoku-solver.exe", "--grid", strings.Join(input, ","))
+
+		output, err := cmd.Output()
+		if err != nil {
+			fmt.Println(string(output))
+			t.Fatal(err)
+		}
+
+		if !strings.Contains(string(output), "The grid has been solved!") {
+			t.Errorf("Grid should have been solved, but was not: \n" + string(output))
+		}
 	})
 }
