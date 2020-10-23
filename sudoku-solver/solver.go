1acf82d68996cc5e589bd17a3dfc1da1d7bf7084
yarhamjohn <yarhamjohn@gmail.com>
2020-10-23 16:53:39 +0100

    wip - try next bit of logic

--- sudoku-solver/solver.go
+++ sudoku-solver/solver.go
@@ -45,21 +45,9 @@ func updateGrid(grid *grid, value string) bool {
 }
 
 func updateUnit(row []*square, value string) (bool, error) {
-	var solvedSquare *square
-	var squaresWithValue []*square
-	var squaresWithoutValue []*square
-	for _, square := range row {
-		if square.getValue() == value {
-			if solvedSquare == nil {
-				solvedSquare = square
-			} else {
-				return false, errors.New("multiple squares are solved with the same value")
-			}
-		} else if square.isPossibleValue(value) {
-			squaresWithValue = append(squaresWithValue, square)
-		} else {
-			squaresWithoutValue = append(squaresWithoutValue, square)
-		}
+	solvedSquare, squaresWithValue, squaresWithoutValue, err := categorizeSquares(row, value)
+	if err != nil {
+		return false, err
 	}
 
 	// One square has no other possible values so all other squares can exclude this as a possible value
@@ -89,7 +77,67 @@ func updateUnit(row []*square, value string) (bool, error) {
 		}
 	}
 
-	return false, nil
+	// Multiple squares have the value as possible forming a mini-group if non-matching squares that contain 1 or more other identical possible values not present in the nonMatchingSquares meaning all other values can be excluded from all other squares
+	var nonMatchingSquaresPossibleValues []string // contains unique list of possible values from the squares that did not have the value provided
+	for _, square := range squaresWithoutValue {
+		for _, val := range square.possibleValues {
+			for _, possVal := range nonMatchingSquaresPossibleValues {
+				if val == possVal {
+					break
+				}
+			}
+
+			nonMatchingSquaresPossibleValues = append(nonMatchingSquaresPossibleValues, val)
+		}
+	}
+
+	var valContainingSquaresWithOtherPossibleValue []*square // contains list of all squares containing the value provided and another possible value that was not possible for any squares without the provided value
+	var interestingVals []string
+	var unInterestingVals []string
+	for _, square := range squaresWithValue {
+		for _, val := range square.possibleValues {
+			for _, possVal := range nonMatchingSquaresPossibleValues {
+				if val == possVal {
+					unInterestingVals = append(unInterestingVals, val)
+					break
+				}
+			}
+
+			// Both of these need to only add if unique
+			valContainingSquaresWithOtherPossibleValue = append(valContainingSquaresWithOtherPossibleValue, square)
+			interestingVals = append(interestingVals, val)
+		}
+	}
+
+	if len(valContainingSquaresWithOtherPossibleValue) == len(interestingVals) {
+		for _, square := range valContainingSquaresWithOtherPossibleValue {
+			for _, val := range unInterestingVals {
+				valueExcluded = square.exclude(val)
+			}
+		}
+	}
+
+	return valueExcluded, nil
+}
+
+func categorizeSquares(row []*square, value string) (*square, []*square, []*square, error) {
+	var solvedSquare *square
+	var squaresWithValue []*square
+	var squaresWithoutValue []*square
+	for _, square := range row {
+		if square.getValue() == value {
+			if solvedSquare == nil {
+				solvedSquare = square
+			} else {
+				return nil, nil, nil, errors.New("multiple squares are solved with the same value")
+			}
+		} else if square.isPossibleValue(value) {
+			squaresWithValue = append(squaresWithValue, square)
+		} else {
+			squaresWithoutValue = append(squaresWithoutValue, square)
+		}
+	}
+	return solvedSquare, squaresWithValue, squaresWithoutValue, nil
 }
 
 // Determine which squares in a slice have a matching set of possible values vs a non-matching set and return buckets
